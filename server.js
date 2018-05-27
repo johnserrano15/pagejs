90020824bed8b4248e9db384825fbb9bb99c7194
john andrey <andrey_201015@hotmail.com>
2018-05-27 11:06:16 -0500

    Cambios para el ejemplo del blog.johnserrano.co

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.set('view engine', 'pug');
 app.use(express.static('public'));
-// app.use(express.static('./'));
 
 app.get('/', function (req, res) {
-  res.render('index', { title: 'Uso de pagejs', message: 'Hello there!'});
+  res.render('index');
   // res.status(200).send('Hola mundo todo bien')
 })
 
